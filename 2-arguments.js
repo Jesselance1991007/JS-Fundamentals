24187453c700a029b70ef2bda931463d5e9aa89a
ADEYEMO ABIODUN OLUKAYODE <Jesselance1991@gmail.com>
2025-05-25 09:21:08 +0100

    Add: script that checks number of arguments

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const args = process.argv.length;
+if (args===2){
+    console.log("No argument");
+} else if (args ===3){
+    console.log("Argument");
+}else {
+    console.log("Arguments found");
+}
