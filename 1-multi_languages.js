fe2bf26f63141cd345a02ac641c9412e91b74275
ADEYEMO ABIODUN OLUKAYODE <Jesselance1991@gmail.com>
2025-05-25 08:45:46 +0100

    Add: print 3 lines using console.log and let

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
-const line1 = "C is fun";
-const line2 = "Python is cool";
-const line3 = "Javascript is amazing";
+ let line1 = "C is fun";
+ let line2 = "Python is cool";
+ let line3 = "Javascript is amazing";
 
 console.log(line1);
 console.log(line2);
